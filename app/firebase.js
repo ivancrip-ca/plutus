2dfc716e679ab74143b29994119d242ac16e7dc0
ClicSafe <corpclicsafe@gmail.com>
2025-04-22 13:20:40 -0600

    Se quitan las claves de firebase

--- app/firebase.js
+++ app/firebase.js
@@ -2,19 +2,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
-
+import { getFirestore } from "firebase/firestore";
+import { enableIndexedDbPersistence } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCDsX3YjdLydPmzg941_7ewXw2yIk7EKZ0",
-  authDomain: "plutus-93ccc.firebaseapp.com",
-  projectId: "plutus-93ccc",
-  storageBucket: "plutus-93ccc.appspot.com", // Corregido el nombre del bucket de almacenamiento
-  messagingSenderId: "660937233999",
-  appId: "1:660937233999:web:62cd8b3559891af76a7c3a",
-  measurementId: "G-6EN1T1K8F0"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
